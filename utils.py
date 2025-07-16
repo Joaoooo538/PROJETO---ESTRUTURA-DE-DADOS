15f3ffc81844c4d05a7793c914c26a660868dfa7
Carlos Vinícius <162610513+CaviSantos@users.noreply.github.com>
2025-07-16 20:03:30 -0300

    melhorias utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
+import os
+import platform
+
 def menu_principal():
-    print("\n===== Sistema de Biblioteca =====")
+    clear_console()
+    print("="*35)
+    print("     SISTEMA DE BIBLIOTECA")
+    print("="*35)
     print("1. Cadastrar Livro")
     print("2. Cadastrar Usuário")
     print("3. Emprestar Livro")
@@ -7,4 +13,19 @@ def menu_principal():
     print("5. Buscar Livro")
     print("6. Relatório de Empréstimos")
     print("0. Sair")
-    return input("Escolha uma opção: ")
+    print("="*35)
+
+    opcao = input("Escolha uma opção: ").strip()
+    return opcao
+
+def clear_console():
+
+    sistema = platform.system()
+    if sistema == "Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+def pausar():
+
+    input("\nPressione Enter para continuar...")
