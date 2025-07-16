5a748800fb251bedafce6f964c9b8c430ab680ec
Carlos Vinícius <162610513+CaviSantos@users.noreply.github.com>
2025-07-16 20:04:05 -0300

    Serialização main.py

--- main.py
+++ main.py
@@ -4,25 +4,30 @@ from utils import menu_principal
 def main():
     biblioteca = Biblioteca()
 
-    while True:
-        opcao = menu_principal()
-        if opcao == "1":
-            biblioteca.cadastrar_livro()
-        elif opcao == "2":
-            biblioteca.cadastrar_usuario()
-        elif opcao == "3":
-            biblioteca.emprestar_livro()
-        elif opcao == "4":
-            biblioteca.devolver_livro()
-        elif opcao == "5":
-            biblioteca.buscar_livro()
-        elif opcao == "6":
-            biblioteca.relatorio_emprestimos()
-        elif opcao == "0":
-            print("Encerrando o sistema...")
-            break
-        else:
-            print("Opção inválida.")
-
+    try:
+        while True:
+            opcao = menu_principal()
+            if opcao == "1":
+                biblioteca.cadastrar_livro()
+            elif opcao == "2":
+                biblioteca.cadastrar_usuario()
+            elif opcao == "3":
+                biblioteca.emprestar_livro()
+            elif opcao == "4":
+                biblioteca.devolver_livro()
+            elif opcao == "5":
+                biblioteca.buscar_livro()
+            elif opcao == "6":
+                biblioteca.relatorio_emprestimos()
+            elif opcao == "0":
+                biblioteca.salvar_dados()
+                print("Salvando Dados...")
+                print("Encerrando o sistema...")
+                break
+            else:
+                print("Opção inválida.")
+    except KeyboardInterrupt:
+        print("Programa interrompido manualmente. Salvando Dados...")
+        biblioteca.salvar_dados()
 if __name__ == "__main__":
     main()
