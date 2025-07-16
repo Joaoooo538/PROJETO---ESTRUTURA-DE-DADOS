9a7deb14071faece3c9ddbf7924b470573a77011
Carlos Vinícius <162610513+CaviSantos@users.noreply.github.com>
2025-07-16 20:04:41 -0300

    Update livro.py

--- livro.py
+++ livro.py
@@ -1,6 +1,6 @@
 class Livro:
-    def __init__(self, titulo, autor, codigo):
+    def __init__(self, titulo, autor, codigo, emprestado = False):
         self.titulo = titulo
         self.autor = autor
         self.codigo = codigo
-        self.emprestado = False
+        self.emprestado = emprestado
